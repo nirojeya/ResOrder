f794ede233f06c71458dd81462aae5a7ea477471
dev1 <t.kunalan18@gmail.com>
2019-03-09 12:35:44 +0530

    Basic settings

--- app/src/main/java/andros/tc/resorder/MainActivity.java
+++ app/src/main/java/andros/tc/resorder/MainActivity.java
@@ -1,17 +1,53 @@
 package andros.tc.resorder;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import andros.tc.resorder.helper.Utils;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static android.support.v4.app.FragmentManager fragmentManager;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragmentManager = getSupportFragmentManager();
+
+
 
         //test/
         //noodles
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Find the tag of signup fragment
+        Fragment SignUp_Fragment = fragmentManager
+                .findFragmentByTag(Utils.SignupFragment);
+
+        // Check if null or not
+        // If not null then replace login fragment else do backpressed
+        // task
+
+        if (SignUp_Fragment != null)
+            replaceLoginFragment();
+        else
+            super.onBackPressed();
+
+    }
+
+    // Replace Login Fragment with animation
+    protected void replaceLoginFragment() {
+        fragmentManager
+                .beginTransaction()
+                //.setCustomAnimations(R.anim.left_enter, R.anim.right_out)
+                .replace(R.id.frameContainer, new LoginFragment(),
+                        Utils.LoginFragment).commit();
     }
 }
---
+++ app/src/main/java/andros/tc/resorder/helper/Utils.java
@@ -0,0 +1,11 @@
+package andros.tc.resorder.helper;
+
+public class Utils {
+    //Email Validation pattern
+    public static final String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+
+    //Fragments Tags
+    public static final String LoginFragment= "LoginFragment";
+    public static final String SignupFragment = "SignupFragment";
+    //public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+}
