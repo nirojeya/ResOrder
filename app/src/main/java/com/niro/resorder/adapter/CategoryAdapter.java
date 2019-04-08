8dd3762df619e3efc6566e137cc928f63d64fa99
dev1 <t.kunalan18@gmail.com>
2019-04-08 19:14:48 +0530

    ActivityItemSelection.java : fixed issue : fixed issue

--- app/src/main/java/com/niro/resorder/adapter/CategoryAdapter.java
+++ app/src/main/java/com/niro/resorder/adapter/CategoryAdapter.java
@@ -5,18 +5,27 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.niro.resorder.R;
-import com.niro.resorder.pojo.Item;
 
 import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryHolder> {
 
-    List<Item> itemList;
-    public CategoryAdapter(List<Item> itemList){
-        this.itemList = itemList;
+    public interface CategoryClickDelegate{
+        void categoryOnClick(int position);
     }
+
+    private List<String> categoryList;
+    private CategoryClickDelegate categoryClickDelegate;
+
+    public CategoryAdapter(List<String> categoryList,CategoryClickDelegate categoryClickDelegate){
+        this.categoryList = categoryList;
+        this.categoryClickDelegate = categoryClickDelegate;
+    }
+
     @NonNull
     @Override
     public CategoryHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -26,19 +35,29 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CategoryHolder categoryHolder, int i) {
+    public void onBindViewHolder(@NonNull CategoryHolder categoryHolder, final int i) {
+        categoryHolder.categoryName.setText(categoryList.get(i));
 
+        categoryHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                categoryClickDelegate.categoryOnClick(i);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return itemList.size();
+        return categoryList.size();
     }
 
     class CategoryHolder extends RecyclerView.ViewHolder{
-
+        LinearLayout linearLayout;
+        TextView categoryName;
         public CategoryHolder(@NonNull View itemView) {
             super(itemView);
+            linearLayout = itemView.findViewById(R.id.category_root);
+            categoryName = itemView.findViewById(R.id.category_name);
         }
     }
 }
--- app/src/main/java/com/niro/resorder/service/VolleyGetService.java
+++ app/src/main/java/com/niro/resorder/service/VolleyGetService.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -18,13 +19,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class VolleyGetService {
     private static ItemDelegate itemDelegate;
+    private static CategoryDelegate categoryDelegate;
+
     private static Context context;
+    private static List<String> categoryList;
+
 
     public interface ItemDelegate{
         void syncItemDetails(List<Item> itemList);
+    }
+
+    public interface CategoryDelegate{
+        void syncItemCategory(List<String> syncCategoryList);
     }
 
     public static void syncItem(Context con, String url, ItemDelegate i) {
@@ -89,5 +99,67 @@ public class VolleyGetService {
 
         VolleySingleton.getmInstance(context.getApplicationContext()).addToRequestQueue(jsonArrayRequest);
     }
+
+    public static void syncAllItemCategory(Context con,String url, final CategoryDelegate delegate) {
+        context = con;
+        categoryList = new ArrayList<>();
+        categoryDelegate = delegate;
+
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //Log.e("33333333333"," erankirantaa "  );
+                try {
+
+                    JSONArray jsonArray = response.getJSONArray("categories");
+
+                    if (categoryList.size() > 0) {
+                        categoryList.clear();
+
+                    }
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+
+                        if (!object.isNull("category")) {
+                            categoryList.add(object.getString("category"));
+                        }
+                    }
+
+                    categoryDelegate.syncItemCategory(categoryList);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("errrrrrrrrrrr "," " + e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("RES_ERR",error.toString());
+
+                if(error.toString().trim().equalsIgnoreCase("com.android.volley.TimeoutError") ||
+                        error.toString().trim().equalsIgnoreCase("com.android.volley.NoConnectionError: java.net.SocketException: Network is unreachable")) {
+                    // Log.e("respomce_order_err","come error");
+                   // CustomDialog.imeOutExceptionDialog(context);
+                }
+
+            }
+
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                //return setHeaderData();
+            }
+        };
+
+        // now volley retry policy is 20s
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                (int) TimeUnit.SECONDS.toMillis(20000),
+                0,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        VolleySingleton.getmInstance(context.getApplicationContext()).addToRequestQueue(jsonArrayRequest);
+    }
+
 
 }
